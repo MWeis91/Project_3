a55a32d1c8d0fb97db570489b2ce1db4f9dccffd
Matthew Weis <weis@mail.usf.edu>
2014-04-22 19:38:38 -0400

    Added Project

---
+++ Project 3/Project 3/Game.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_3
+{
+    class Game
+    {
+        public int target;
+        public bool outcome;
+
+        aDie Dice1;
+        aDie Dice2;
+
+
+
+        public void game()
+        {
+            
+            int sum = Dice1.Roll() + Dice2.Roll();
+
+            if (sum == 7 || sum == 11)
+                outcome = true;
+            else if (sum == 2 || sum == 3 || sum == 12)
+                outcome = false;
+            else
+            {
+                target = sum;
+
+                while (true)
+                {
+                    sum = Dice1.Roll() + Dice2.Roll();
+
+                    if (sum == 7)
+                    {
+                        outcome = false;
+                        break;
+                    }
+                    else if (sum == target)
+                    {
+                        outcome = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Project 3/Project 3/aDie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_3
+{
+   public class aDie : Random//subclass that simulates a die. Uses Random as a base class.
+    {
+       public int seed;//Stores the seed for the random number generator.
+
+       private const int one = 1;//Privatly stores the lowest value for a dice roll;
+       private const int seven = 7;//Privatly stores the highest value for a dice roll;
+
+       /// <summary>
+       /// Method to roll a die. Returns the value as an int.
+       /// </summary>
+       /// <returns></returns>
+       public int Roll()
+       {
+           Random diceRoll = new Random(seed);//Creates a new random number generator with the specified seed.
+           int rollValue = diceRoll.Next(one, seven);//Rolls the dice, producing an integer between 1 and 6.
+
+           return rollValue;//returns the dices value
+       }
+    }
+}
